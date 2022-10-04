16cb4ca93acd60d81a067f401b200a3ae678d7ca
frendysanusi05 <92145413+frendysanusi05@users.noreply.github.com>
2022-10-04 22:15:01 +0700

    Delete JatisTugas1 directory

--- JatisTugas1/database_login.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE login;
-USE login;
-CREATE TABLE USER(id INT auto_increment PRIMARY KEY, username TEXT, password TEXT);
-INSERT INTO USER(username, password)
-VALUES ('Frendy', '12345678');
